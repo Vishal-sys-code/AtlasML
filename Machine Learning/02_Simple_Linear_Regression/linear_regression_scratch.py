c76415a2de64c1358575d7468c69ea95e3ac4980
Vishal Pandey <68536727+Vishal-sys-code@users.noreply.github.com>
2025-06-15 09:08:08 +0530

    Update linear_regression_scratch.py

---
+++ Machine Learning/02_Simple_Linear_Regression/linear_regression_scratch.py
@@ -0,0 +1,10 @@
+# Importing Libraries
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+# Loading Dataset
+df = pd.read_csv('./datasets/placements.csv')
+
+
