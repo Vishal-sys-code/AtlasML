193c859e76217d768d7f64a158399975356026c1
Vishal Pandey <68536727+Vishal-sys-code@users.noreply.github.com>
2025-06-19 21:35:49 +0530

    Update model.py

--- 03_Transformers [Full Explanation]/Transformer Implementation [Scratch]/model.py
+++ 03_Transformers [Full Explanation]/Transformer Implementation [Scratch]/model.py
@@ -1,2 +1,44 @@
 import torch
 import torch.nn as nn
+import math
+
+# Encoder Architecture
+"""
+Encoder has the architecture consisting of multiple layers that are:
+1. Input Embeddings
+2. Positional Encodings
+3. Multi-Head Self-Attention
+4. Add & Layer Normalization
+5. Feed Forward Neural Network
+6. Add & Layer Normalization
+7. Output of the Encoder
+The output of the encoder is a set of continuous representations of the input tokens.
+
+In the paper, they have use 6 layers of the encoder, this is the meaning of the Nx in the paper.
+The Nx means that the encoder has 6 layers, each layer has the same architecture.
+"""
+
+# Layer 1: Input Embeddings
+class InputEmbeddings(nn.Module):
+
+    def __init__(self, dimension_model:int, vocabulary_size: int):
+        """
+        dimension_model: The dimension of the model, typically 512 or 768.
+        vocabulary_size: The size of the vocabulary, which is the number of unique tokens in the input data.
+
+        The Embeddings means that it will map the input tokens to a continuous vector space.
+        Pytorch provide an `nn.Embedding` layer that does this mapping.
+        The `nn.Embedding` layer takes two arguments: the size of the vocabulary and the dimension of the model.
+        """
+        super().__init__()
+        self.dimension_model = dimension_model
+        self.vocabulary_size = vocabulary_size
+        self.embedding = nn.Embedding(vocabulary_size, dimension_model)
+
+    def forward(self, x):
+        """
+        In the paper, Section 3.4: In the embedding layers, we multiply those weights by sqrt(dimension_model)
+        to scale the input embeddings.
+        x: The input tokens, which are integers representing the tokens in the input data.
+        """
+        return self.embedding(x) * math.sqrt(self.dimension_model)
