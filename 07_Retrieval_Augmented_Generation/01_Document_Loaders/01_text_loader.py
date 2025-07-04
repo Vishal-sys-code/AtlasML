996aaf465439bc5605ede37549e7abece91cec15
Vishal Pandey <68536727+Vishal-sys-code@users.noreply.github.com>
2025-07-04 16:07:18 +0530

    Update 01_text_loader.py

--- 07_Retrieval_Augmented_Generation/01_Document_Loaders/01_text_loader.py
+++ 07_Retrieval_Augmented_Generation/01_Document_Loaders/01_text_loader.py
@@ -1,6 +1,6 @@
 import openai
 from langchain_community.document_loaders import TextLoader 
-from langchain_openai import ChatOpenAI
+# from langchain_openai import ChatOpenAI
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import PromptTemplate
 from dotenv import load_dotenv
