e1c253e67ed84c014da0b12941535670ecfcb230
Vishal Pandey <68536727+Vishal-sys-code@users.noreply.github.com>
2025-07-04 15:57:05 +0530

    Update 01_text_loader.py

--- 07_Retrieval_Augmented_Generation/01_Document_Loaders/01_text_loader.py
+++ 07_Retrieval_Augmented_Generation/01_Document_Loaders/01_text_loader.py
@@ -1,4 +1,26 @@
+import openai
 from langchain_community.document_loaders import TextLoader 
+from langchain_openai import ChatOpenAI
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import PromptTemplate
+from dotenv import load_dotenv
+
+load_dotenv()
+
 loader = TextLoader('../../00_Datasets/cricket.txt', encoding = 'utf-8')
 docs = loader.load()
-print(docs)
+
+model = ChatOpenAI()
+prompt =  PromptTemplate(
+    template = 'Write a summary for the following poem - \n {poem}',
+    input_variables=['poem']
+)
+parser = StrOutputParser()
+chain = prompt | model | parser
+
+# Invoke the chain with the document's content and store the result
+summary = chain.invoke({'poem': docs[0].page_content})
+
+# Print the generated summary
+print("\n--- Generated Summary ---\n")
+print(summary)
