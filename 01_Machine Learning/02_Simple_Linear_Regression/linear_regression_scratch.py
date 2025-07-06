76ca48a63df437257882004eb05bf07a12f23b72
Vishal Pandey <68536727+Vishal-sys-code@users.noreply.github.com>
2025-07-06 10:38:14 +0530

    Update linear_regression_scratch.py

--- 01_Machine Learning/02_Simple_Linear_Regression/linear_regression_scratch.py
+++ 01_Machine Learning/02_Simple_Linear_Regression/linear_regression_scratch.py
@@ -14,3 +14,15 @@ y = df.iloc[:,1].values
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
+# Implementation of Simple Linear Regression
+"""
+
+"""
+
+class SimpleLinearRegression:
+    def __init__():
+        pass
+    def fit():
+        pass
+    def predict():
+        pass
